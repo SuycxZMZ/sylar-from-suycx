287c375fbd6986f127f77b196119c5138caf1e3b
SuycxZMZ <suycx65@163.com>
2024-04-15 20:19:45 +0800

    fix fiber.cc

---
+++ build/include/src/CMakeFiles/sylar-ucontext.dir/DependInfo.cmake
@@ -0,0 +1,28 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/codeCpp/sylar-from-suycx/include/src/fiber.cc" "/home/codeCpp/sylar-from-suycx/build/include/src/CMakeFiles/sylar-ucontext.dir/fiber.cc.o"
+  "/home/codeCpp/sylar-from-suycx/include/src/logger.cc" "/home/codeCpp/sylar-from-suycx/build/include/src/CMakeFiles/sylar-ucontext.dir/logger.cc.o"
+  "/home/codeCpp/sylar-from-suycx/include/src/timestamp.cc" "/home/codeCpp/sylar-from-suycx/build/include/src/CMakeFiles/sylar-ucontext.dir/timestamp.cc.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_CXX
+  "sylar_ucontext_EXPORTS"
+  )
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../include"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ build/include/src/CMakeFiles/sylar-ucontext.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "../../../lib/libsylar-ucontext.pdb"
+  "../../../lib/libsylar-ucontext.so"
+  "CMakeFiles/sylar-ucontext.dir/fiber.cc.o"
+  "CMakeFiles/sylar-ucontext.dir/logger.cc.o"
+  "CMakeFiles/sylar-ucontext.dir/timestamp.cc.o"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/sylar-ucontext.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
